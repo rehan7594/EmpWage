115b970b21267bb347848791875e228c91bcc20d
Rehan <rehansdm94@gmail.com>
2021-03-26 02:50:04 +0530

    Added: Absent and present empWage

--- Empwage.sh
+++ Empwage.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+empCheck=$((RANDOM%2))
+isPresent=1
+if [ $empCheck -eq $isPresent ]
+then
+        echo "employee is Present"
+else
+        echo "Employee is absent"
+fi
 
-echo "Welcome to EmpWage"
